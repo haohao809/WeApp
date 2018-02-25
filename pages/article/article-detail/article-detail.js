4ff248966143744c7a7d451f82612caf847f610b
haohao809 <haohao809@126.com>
2018-02-25 16:05:28 +0800

    storage

--- pages/article/article-detail/article-detail.js
+++ pages/article/article-detail/article-detail.js
@@ -7,7 +7,50 @@ Page({
 		this.setData({
 			postData: postData
 		})
-	}
 
+		var postsCollected = wx.getStorageSync('posts_collected')
+		if(postsCollected){
+			var postCollected = postsCollected[postId];
+			this.setData({
+				collected: postCollected
+			})
+		} else {
+			var postsCollected = {};
+			postsCollected[postId] = false;
+			wx.setStorageSync('posts_collected',postsCollected);
+		}
+	},
+	onColletionTap: function (event){
+		this.getPostsCollectedAsy();
+	},
+	getPostsCollectedAsy: function(){
+		var that = this;
+		wx.getStorage({
+			key: "posts_collected",
+			success: function (res) {
+				var postsCollected = res.data;
+				var postCollected = postsCollected[that.data.currentPostId];
 
+				postCollected = !postCollected;
+				postsCollected[that.data.currentPostId] = postCollected;
+
+				that.showToast(postsCollected,postCollected);
+			}
+
+		})
+
+	},
+	showToast:function(postsCollected,postCollected){
+		wx.setStorageSync('posts_collected', postsCollected);
+
+		this.setData({
+			collected: postCollected
+		}) 
+
+		wx.showToast({
+			title: postCollected ? "收藏成功" : "取消成功",
+			duration: 1000,
+			icon: "success"
+		})
+	},
 })

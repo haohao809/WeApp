5f682f9b5d8228dfcfaf88564ce62bbe70a30544
haohao809 <haohao809@126.com>
2018-02-28 17:39:47 +0800

    storage

--- pages/article/article-detail/article-detail.js
+++ pages/article/article-detail/article-detail.js
@@ -17,7 +17,7 @@ Page({
 		var postsCollected = wx.getStorageSync('posts_collected')
 		if(postsCollected){
 			var postCollected = postsCollected[postId];
-			if(postCollected){
+			if(postCollected){ 
 				this.setData({
 				collected: postCollected
 			})
--- pages/article/article.js
+++ pages/article/article.js
@@ -1,23 +1,61 @@
 var Data = require('../../data/article-data.js')
 Page({
   data:{
-
+  	 postId: 0,
   },
 	onLoad: function () {
 		this.setData({
 			postList:Data.postList
 		});
+		var articleReadNum = wx.getStorageSync('article_num')
+		if(articleReadNum){
+			for(var j=0; j<Data.postList.length;j++){
+				for(var o in articleReadNum){
+					if(parseInt(o) === Data.postList[j].postId){
+						this.data.postList[o].reading = articleReadNum[o]
+						this.setData({
+							postList:this.data.postList
+						});	
+					  // console.log('this.data.postList[o].reading',this.data.postList[o].reading) 					
+					}
+				}
+			}	
+			console.log(this.data.postList);
+		}else{
+			var articleReadNum = {}
+			var list = Data.postList;
+			console.log(list);
+			for (var i = list.length - 1; i >= 0; i--) {
+				articleReadNum[list[i].postId] = list[i].reading;
+			}
+			console.log(articleReadNum);
+			wx.setStorageSync('article_num',articleReadNum);
+		}
 	},
 	itemDetail: function(event){
-		let postId = event.currentTarget.dataset.postid;
+		console.log(this.postId);
+		this.postId = event.currentTarget.dataset.postid;
+		this.setData({
+			postId : this.postId
+		})
+		this.saveStorage(this.postId);
 		wx.navigateTo({
-			url: "article-detail/article-detail?id=" + postId
+			url: "article-detail/article-detail?id=" + this.postId
 		})
 	},
 	onSwiperTap: function(event){
-		let postId = event.target.dataset.postid;
-		wx.navigateTo({
-			url: "article-detail/article-detail?id=" + postId
+		this.postId = event.target.dataset.postid;
+		this.setData({
+			postId : this.postId
 		})
+		this.saveStorage(this.postId);
+		wx.navigateTo({
+			url: "article-detail/article-detail?id=" + this.postId
+		})
+	},
+	saveStorage: function(id){
+		var articleReadNum = wx.getStorageSync('article_num');
+		articleReadNum[id]++;
+		wx.setStorageSync('article_num',articleReadNum);
 	}
 }) 

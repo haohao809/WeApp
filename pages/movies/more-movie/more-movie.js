e4237722396d2c9ffcb6230b14d5fe0deb195b70
haohao809 <haohao809@126.com>
2018-03-17 22:55:06 +0800

    more

--- pages/movies/more-movie/more-movie.js
+++ pages/movies/more-movie/more-movie.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    navigateTitle: ""
   },
 
   /**
@@ -13,14 +13,20 @@ Page({
    */
   onLoad: function (options) {
     let name = options.titleName;
+    this.data.navigateTitle = name;
     console.log(name);
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
+  onReady: function (event) {
+      wx.setNavigationBarTitle({
+        title:  this.data.navigateTitle,
+        success: function(res) {
+
+        }
+      })
   },
 
   /**

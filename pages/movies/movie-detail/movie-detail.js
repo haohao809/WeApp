287bba3a36ebb700bb6b988d5f1d7ba5ba9206ee
haohao809 <haohao809@126.com>
2018-03-29 22:36:29 +0800

    class

--- pages/movies/movie-detail/movie-detail.js
+++ pages/movies/movie-detail/movie-detail.js
@@ -1,5 +1,6 @@
 // pages/movies/movie-detail/movie-detail.js
 var util = require('../../../utils/util.js')
+import { Movie } from 'class/Movie.js'
 console.log(util);
 var app = getApp();
 Page({
@@ -19,47 +20,53 @@ Page({
       let movieId = options.id;
       let url = app.globalData.doubanBase + 
       "/v2/movie/subject/" + movieId;
-			util.http(url,this.processData);
+      let movie = new Movie(url);
+      movie.getMovieData((movie)=>{
+          this.setData({
+            movie:movie
+          })
+      })
+			// util.http(url,this.processData);
   },
-  processData(data){
-  	console.log(data);
-  	if (!data) {
-            return;
-        }
-        var director = {
-            avatar: "",
-            name: "",
-            id: ""
-        }
-       var  data = data.data;
-        if (data.directors[0] != null) {
-            if (data.directors[0].avatars != null) {
-                director.avatar = data.directors[0].avatars.large
+  // processData(data){
+  // 	console.log(data);
+  // 	if (!data) {
+  //           return;
+  //       }
+  //       var director = {
+  //           avatar: "",
+  //           name: "",
+  //           id: ""
+  //       }
+  //      var  data = data.data;
+  //       if (data.directors[0] != null) {
+  //           if (data.directors[0].avatars != null) {
+  //               director.avatar = data.directors[0].avatars.large
 
-            }
-            director.name = data.directors[0].name;
-            director.id = data.directors[0].id;
-        }
-        var movie = {
-            movieImg: data.images ? data.images.large : "",
-            country: data.countries[0],
-            title: data.title,
-            originalTitle: data.original_title,
-            wishCount: data.wish_count,
-            commentCount: data.comments_count,
-            year: data.year,
-            generes: data.genres.join("、"),
-            stars: util.changeStar(data.rating.stars),
-            score: data.rating.average,
-            director: director,
-            casts: util.convertToCastString(data.casts),
-            castsInfo: util.convertToCastInfos(data.casts),
-            summary: data.summary
-        }
-        this.setData({
-        	movie:movie
-        })
-  },
+  //           }
+  //           director.name = data.directors[0].name;
+  //           director.id = data.directors[0].id;
+  //       }
+  //       var movie = {
+  //           movieImg: data.images ? data.images.large : "",
+  //           country: data.countries[0],
+  //           title: data.title,
+  //           originalTitle: data.original_title,
+  //           wishCount: data.wish_count,
+  //           commentCount: data.comments_count,
+  //           year: data.year,
+  //           generes: data.genres.join("、"),
+  //           stars: util.changeStar(data.rating.stars),
+  //           score: data.rating.average,
+  //           director: director,
+  //           casts: util.convertToCastString(data.casts),
+  //           castsInfo: util.convertToCastInfos(data.casts),
+  //           summary: data.summary
+  //       }
+  //       this.setData({
+  //       	movie:movie
+  //       })
+  // },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
